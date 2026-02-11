04f794074f9eb280779f27f604253dbdae135b21
крысавдоме <krisanevdome@gmail.com>
2026-02-11 21:40:55 +0300

    start migrartion

--- src/http/ui/src/barrels/fields.ts
+++
@@ -1,6 +0,0 @@
-export * from "@components/common/B4TextField";
-export * from "@components/common/B4Select";
-export * from "@components/common/B4FormGroup";
-export * from "@components/common/B4Switch";
-export * from "@components/common/B4Slider";
-export * from "@components/common/B4RangeSlider";
